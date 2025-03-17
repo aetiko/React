92e043ca7f7794eb180ffee456c5e633461b02b4
ayo <nurudeenetiko12@gmail.com>
2025-03-17 10:20:46 -0600

    add winner function to determine winner of game

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -11,13 +11,13 @@ export default function Board() {
   const [isNext, setIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   function handleClick(i) {
-    if(squares[i]){
+    if (squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
-    if(isNext){
+    if (isNext) {
       nextSquares[i] = "X";
-    }else {
+    } else {
       nextSquares[i] = "O";
     }
     setSquares(nextSquares);
@@ -42,4 +42,23 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
 }
